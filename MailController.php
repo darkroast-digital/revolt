4eae1e6d2d9bc304e6e7aa06aaefe44368eafc56
Kim Morin <kimmorin93@gmail.com>
2017-12-13 10:38:57 -0500

    fixed mail controller

--- MailController.php
+++ MailController.php
@@ -1,44 +1,72 @@
 <?php 
 
 require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
+//use Mailgun\Mailgun;
+use PHPMailer\PHPMailer\PHPMailer;
 
 
 function sendToRevolt() {
 
-    $email = $_POST['email'];
-    $name = $_POST['name'];
+        $mail = new PHPMailer;
 
-    $mg = Mailgun::create('key-1715c074f053673f6e3c4c79e8595390');
-    # Now, compose and send your message.
-    # $mg->messages()->send($domain, $params);
-    $mg->messages()->send('revoltdigital.ca', [
-      'from'    => $email,
-      'to'      => 'contact@revoltdigital.ca',
-      'subject' => 'New Revolt Subscriber',
-      'html'    => $name . " at " . $email . " would like to receive the Revolt presentation on digital currency, e-commerce, and e-learning."
-    ]);
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        
+        $subject = "New Revolt Subscriber";
+
+        $mail->setFrom($email, $name);
+        $mail->addAddress('contact@revoltdigital.ca', 'Revolt Digital');
+        $mail->addReplyTo('contact@revoltdigital.ca', 'Revolt Digital');
+        $mail->ReturnPath='contact@revoltdigital.ca';
+
+        $mail->isHTML(true);
+
+        $body = $name . ' at ' . $email . ' has just subscribed to receive a free presentation on digital currency, e-commerce, and e-learning.';
+
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = $body;
+
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Success!';
+        }
 
 }
 
 function sendToUser() {
 
-    $email = $_POST['email'];
-    $name = $_POST['name'];
+      $mail = new PHPMailer;
 
-    $mg = Mailgun::create('key-1715c074f053673f6e3c4c79e8595390');
-    # Now, compose and send your message.
-    # $mg->messages()->send($domain, $params);
-    $mg->messages()->send('revoltdigital.ca', [
-      'from'    => 'contact@revoltdigital.ca',
-      'to'      => $email,
-      'subject' => 'Thanks for subscribing!',
-      'html'    => "<p>Thank you for your interest in ReVolt Evolution of Digital Currency.  You will receive follow up emails with details on ReVolt Events in your area.</p>
-        <p>Please find us on Facebook at <a href='https://www.facebook.com/revoltdigitalcurrency/'>https://www.facebook.com/revoltdigitalcurrency/</a> for updates and info on the Digital Currency world.</p>
-        <p>Respectfully,</p>
-        <p>Team ReVolt</p>"
-    ]);
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        
+        $subject = "Thanks for subscribing, " . $name . "!";
+
+        $mail->setFrom('contact@revoltdigital.ca');
+        $mail->addAddress($email, $name);
+        $mail->addReplyTo('contact@revoltdigital.ca', 'Revolt Digital');
+        $mail->ReturnPath='contact@revoltdigital.ca';
+
+        $mail->isHTML(true);
+
+        $body = "<p>Thank you for your interest in ReVolt Evolution of Digital Currency.  You will receive follow up emails with details on ReVolt Events in your area.</p>
+                <p>Please find us on Facebook at <a href='https://www.facebook.com/revoltdigitalcurrency/'>https://www.facebook.com/revoltdigitalcurrency/</a> for updates and info on the Digital Currency world.</p>
+                <p>Respectfully,</p>
+                <p>Team ReVolt</p>";
+
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+        $mail->AltBody = $body;
+
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Success!';
+        }
 
 }
 
